e890814bdefcf831e2060db276b14b390a64a8fc
Alexander Elbracht <alexander@elbracht.org>
2019-02-07 13:45:42 +0100

    Implement a simple server to test this Power-Up

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+// your manifest must have appropriate CORS headers, you could also use '*'
+app.use(cors({ origin: '*' }));
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// listen for requests
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Your server is listening on http://localhost:${port}/`);
+});
