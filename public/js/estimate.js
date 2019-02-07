c0d2d4f621c501b85a1d7e710422e8f346e81bc2
Alexander Elbracht <alexander@elbracht.org>
2019-02-07 16:45:42 +0100

    Check input format

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -2,7 +2,25 @@ const t = TrelloPowerUp.iframe();
 
 window.estimate.addEventListener('submit', (event) => {
   event.preventDefault();
-  return t.set('card', 'shared', 'estimateTime', window.estimateTime.value)
+
+  // Check input format
+  const regex = /^(?:^|\s*)(\d+[w|W])?(?:^|\s*)(\d+[d|D])?(?:^|\s*)(\d+[h|H])?(?:^|\s*)$/;
+  const estimateValues = window.estimateTime.value.match(regex);
+
+  // Get error message for wrong input
+  if (estimateValues == null) {
+    t.alert({
+      message: 'The estimated time could not be saved because the input was in an invalid format.',
+      duration: 6,
+      display: 'error',
+    });
+    return;
+  }
+
+  // Unify estimate time
+  const estimateTime = estimateValues.slice(1).filter(Boolean).join(' ').toLowerCase();
+
+  return t.set('card', 'shared', 'estimateTime', estimateTime)
     .then(() => {
       t.closePopup();
     });
