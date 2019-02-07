80bff74669542f35d8523b20335ddf206ecb4275
Alexander Elbracht <alexander@elbracht.org>
2019-02-07 15:17:52 +0100

    Add a second badge to card

--- public/js/client.js
+++ public/js/client.js
@@ -11,5 +11,8 @@ TrelloPowerUp.initialize({
     .then(estimate => [{
       icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
       text: estimate,
+    }, {
+      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+      text: estimate,
     }]),
 });
