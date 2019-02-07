00e7e4c4b661fd4d81dace5bd58b3260915c8b59
Alexander Elbracht <alexander@elbracht.org>
2019-02-07 16:52:05 +0100

    Fix wrong variable to get estimate time

--- public/js/client.js
+++ public/js/client.js
@@ -3,11 +3,11 @@ TrelloPowerUp.initialize({
     icon: './images/estimateTime.png',
     text: 'Estimate Time',
     callback: t => t.popup({
-      title: 'Estimation',
+      title: 'Estimate',
       url: 'estimate.html',
     }),
   }],
-  'card-badges': t => t.get('card', 'shared', 'estimate')
+  'card-badges': t => t.get('card', 'shared', 'estimateTime')
     .then(estimate => [{
       text: estimate ? `Estimate: ${estimate}` : 'No Estimate',
       color: 'light-gray',
