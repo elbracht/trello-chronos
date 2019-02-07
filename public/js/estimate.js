10bf9d51b4e40bf26f0437ad83c70cb41d70ab01
Alexander Elbracht <alexander@elbracht.org>
2019-02-07 17:39:25 +0100

    Parse estimate time for next higher value

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,20 +1,39 @@
 const t = TrelloPowerUp.iframe();
 
+function parseEstimate(estimateValues, workingHours, workingDays) {
+  let hour = estimateValues[3] ? parseInt(estimateValues[3].slice(0, -1), 10) : 0;
+  let day = estimateValues[2] ? parseInt(estimateValues[2].slice(0, -1), 10) : 0;
+  let week = estimateValues[1] ? parseInt(estimateValues[1].slice(0, -1), 10) : 0;
+
+  if (hour >= workingHours) {
+    day += Math.floor(hour / workingHours);
+    hour %= workingHours;
+  }
+
+  if (day >= workingDays) {
+    week += Math.floor(day / workingDays);
+    day %= workingDays;
+  }
+
+  hour = hour > 0 ? `${hour}h` : 0;
+  day = day > 0 ? `${day}d` : 0;
+  week = week > 0 ? `${week}w` : 0;
+
+  return [week, day, hour].filter(Boolean).join(' ');
+}
+
 window.estimate.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Check input format
   const regex = /^(?:^|\s*)(\d+[w|W])?(?:^|\s*)(\d+[d|D])?(?:^|\s*)(\d+[h|H])?(?:^|\s*)$/;
   const estimateValues = window.estimateTime.value.match(regex);
 
-  // Get an error message for wrong input
   if (estimateValues == null) {
     // TODO: Implement error message for input field
     return;
   }
 
-  // Unify estimate time
-  const estimateTime = estimateValues.slice(1).filter(Boolean).join(' ').toLowerCase();
+  const estimateTime = parseEstimate(estimateValues, 8, 5);
 
   return t.set('card', 'shared', 'estimateTime', estimateTime)
     .then(() => {
