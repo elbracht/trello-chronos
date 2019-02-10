6cfc9ebd137b40fa01c25f4d696f197a3092ac2d
Alexander Elbracht <alexander@elbracht.org>
2019-02-10 21:17:42 +0100

    Add badges for log and remaining time

--- app/components/index.js
+++ app/components/index.js
@@ -2,15 +2,15 @@ import { hoursToTime } from '../helper/time';
 
 TrelloPowerUp.initialize({
   'card-buttons': () => [{
-    icon: './images/estimateTime.png',
     text: 'Estimate Time',
+    icon: './images/estimateTime.png',
     callback: t => t.popup({
       title: 'Estimate Time',
       url: 'estimate.html',
     }),
   }, {
-    icon: './images/logTime.png',
     text: 'Log Time',
+    icon: './images/logTime.png',
     callback: t => t.popup({
       title: 'Log Time',
       url: 'log.html',
@@ -18,11 +18,34 @@ TrelloPowerUp.initialize({
   }],
   'card-badges': t => t.getAll()
     .then((data) => {
+      const badges = [];
+
       const { estimateTime } = data.card.shared;
-      const estimateBadge = {
+      badges.push({
         text: estimateTime ? `Estimate: ${hoursToTime(estimateTime, 5, 8)}` : 'No Estimate',
-        color: 'light-gray',
-      };
-      return [estimateBadge];
+        icon: './images/estimateTime.png',
+      });
+
+      const { logTime } = data.card.shared;
+      badges.push({
+        text: logTime ? `Log: ${hoursToTime(logTime, 5, 8)}` : 'No Log',
+        icon: './images/logTime.png',
+      });
+
+      if (estimateTime && logTime) {
+        let remainingTimeColor;
+        const percentageTime = (100 * logTime) / estimateTime;
+        // TODO: Settings for sections (0 – 50; 50 – 80; 80 – 100)
+        if (percentageTime >= 0 && percentageTime <= 50) remainingTimeColor = 'green';
+        else if (percentageTime > 50 && percentageTime <= 80) remainingTimeColor = 'yellow';
+        else if (percentageTime > 80 && percentageTime <= 100) remainingTimeColor = 'red';
+
+        badges.push({
+          text: `Remaining: ${hoursToTime(estimateTime - logTime, 5, 8)}`,
+          color: remainingTimeColor,
+        });
+      }
+
+      return badges;
     }),
 });
