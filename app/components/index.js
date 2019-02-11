44948ff56cd53a7321ab7e504e74a65f648479c7
Alexander Elbracht <alexander@elbracht.org>
2019-02-11 11:43:55 +0100

    Remove badge icons and fix badge color

--- app/components/index.js
+++ app/components/index.js
@@ -23,22 +23,20 @@ TrelloPowerUp.initialize({
       const { estimateTime } = data.card.shared;
       badges.push({
         text: estimateTime ? `Estimate: ${hoursToTime(estimateTime, 5, 8)}` : 'No Estimate',
-        icon: './images/estimateTime.png',
       });
 
       const { logTime } = data.card.shared;
       badges.push({
         text: logTime ? `Log: ${hoursToTime(logTime, 5, 8)}` : 'No Log',
-        icon: './images/logTime.png',
       });
 
       if (estimateTime && logTime) {
         let remainingTimeColor;
         const percentageTime = (100 * logTime) / estimateTime;
-        // TODO: Settings for sections (0 – 50; 50 – 80; 80 – 100)
-        if (percentageTime >= 0 && percentageTime <= 50) remainingTimeColor = 'green';
+        // TODO: Settings for sections (green < 50 < yellow < 80 < red)
+        if (percentageTime <= 50) remainingTimeColor = 'green';
         else if (percentageTime > 50 && percentageTime <= 80) remainingTimeColor = 'yellow';
-        else if (percentageTime > 80 && percentageTime <= 100) remainingTimeColor = 'red';
+        else if (percentageTime > 80) remainingTimeColor = 'red';
 
         badges.push({
           text: `Remaining: ${hoursToTime(estimateTime - logTime, 5, 8)}`,
